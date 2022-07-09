b423041d55b247734ec6d4a902423d0e0b84dd9e
Julian <perassijulian@gmail.com>
2022-07-09 15:42:55 -0300

    create config.poap.json where we have 100 claim links and /api/poap as dispenser of those links

--- pages/ceramic.js
+++ pages/ceramic.js
@@ -1,11 +1,25 @@
 import Head from '../components/Head'
 import Header from '../components/Header'
-import { createAccountsDocument, updateAccountsDocument, initOrUpdateContacts } from '../lib/ceramicFunctions'
+import {
+  createAccountsDocument,
+  updateAccountsDocument,
+  initOrUpdateContacts,
+} from '../lib/ceramicFunctions'
 
-const ceramic = () => {
+const Ceramic = () => {
+  //THIS PAGE IS FOR TESTING PURPOSES. WHEN CERAMIC INTEGRATION IS WORKING
+  //ITS GONNA BE DELETED. ALSO I'M USING IT FOR OTHER TESTINGS LIKE POAP
+
   const create = async () => {
-    await initOrUpdateContacts(["acc1", "acc2", "acc3","acc4"])
+    await initOrUpdateContacts(['acc1', 'acc2', 'acc3', 'acc4'])
   }
+
+  const getPoapLink = async () => {
+    const res = await fetch('/api/poap')
+    const url = await res.json()
+    console.log(url)
+  }
+
   return (
     <div className="bg-mybg-light dark:bg-mybg-dark min-h-screen flex flex-col ">
       <Head />
@@ -13,6 +27,7 @@ const ceramic = () => {
 
       <main className="z-10 mx-auto max-w-6xl scrollbar-hide my-11 flex-grow ">
         <button onClick={() => create()}>check</button>
+        <button onClick={() => getPoapLink()}>getPoapLink</button>
       </main>
 
       <footer className="mt-auto mb-0"></footer>
@@ -20,4 +35,4 @@ const ceramic = () => {
   )
 }
 
-export default ceramic
+export default Ceramic

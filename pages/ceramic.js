3716a48578ae023955b5b383e459c4df984b6c3f
Julian <perassijulian@gmail.com>
2022-07-09 11:02:59 -0300

    some annotations

--- pages/ceramic.js
+++ pages/ceramic.js
@@ -6,17 +6,13 @@ const ceramic = () => {
   const create = async () => {
     await initOrUpdateContacts(["acc1", "acc2", "acc3","acc4"])
   }
-  const update = async () => {
-    await updateAccountsDocument(["acc1", "acc2", "acc3", "acc4"])
-  }
   return (
     <div className="bg-mybg-light dark:bg-mybg-dark min-h-screen flex flex-col ">
       <Head />
       <Header />
 
       <main className="z-10 mx-auto max-w-6xl scrollbar-hide my-11 flex-grow ">
-        <button onClick={() => create()}>create</button>
-        <button onClick={() => update()}>update</button>
+        <button onClick={() => create()}>check</button>
       </main>
 
       <footer className="mt-auto mb-0"></footer>

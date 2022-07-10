580d6ebbb6c6348c82e38eb1e6d3fb88c0dbb9ae
Julian <perassijulian@gmail.com>
2022-07-10 19:43:07 -0300

    extended functionality to work with array of objects

--- pages/ceramic.js
+++ pages/ceramic.js
@@ -1,11 +1,45 @@
 import Head from '../components/Head'
 import Header from '../components/Header'
-import { createAccountsDocument, updateAccountsDocument, initOrUpdateContacts } from '../lib/ceramicFunctions'
+import {
+  createAccountsDocument,
+  updateAccountsDocument,
+  initOrUpdateContacts,
+} from '../lib/ceramicFunctions'
 
 const ceramic = () => {
   const create = async () => {
-    const doc = await initOrUpdateContacts(["acc1", "acc2","aojd"])
-    console.log('doc:', doc.content);
+    const acc1 = {
+      address: '0x234897',
+      name: 'Annie',
+      notes: 'Annie is the best',
+      tags: ['nft', 'defi'],
+    }
+    const acc2 = {
+      address: '0x123456',
+      name: 'Bob',
+      notes: 'Bob is the best',
+      tags: ['nft'],
+    }
+    const acc3 = {
+      address: '0x654321',
+      name: 'Charlie',
+      notes: 'Charlie is cool',
+      tags: ['gamefy'],
+    }
+    const acc4 = {
+      address: '0x987654',
+      name: 'Daniel',
+      notes: 'Daniel is nice',
+      tags: ['bitcoin'],
+    }
+    const acc5 = {
+      address: '0x456789',
+      name: 'Esteban',
+      notes: 'Esteban is nice',
+      tags: ['bitcoin'],
+    }
+    const doc = await initOrUpdateContacts([acc2, acc5, acc3])
+    console.log('doc:', doc.content)
   }
   return (
     <div className="bg-mybg-light dark:bg-mybg-dark min-h-screen flex flex-col ">

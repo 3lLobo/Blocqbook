48bcebf1118571673d9dbf82ed9c0f5894e53bd9
Reshma <reshmashaik3011@gmail.com>
2022-07-10 17:02:23 +0530

    Landing Page Done

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,16 @@
+import * as React from 'react'
 import '../styles/globals.css'
 import { store } from '../app/store'
 import { Provider } from 'react-redux'
+import { ChakraProvider } from '@chakra-ui/react'
+import { theme } from '../theme'
 
 function MyApp({ Component, pageProps }) {
   return (
     <Provider store={store}>
-      <Component {...pageProps} />
+      <ChakraProvider theme={theme}>
+        <Component {...pageProps} />
+      </ChakraProvider>
     </Provider>
   )
 }

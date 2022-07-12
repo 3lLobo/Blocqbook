abbd470fc1256586e7ddbc1e37760377cc76e453
3lLobo <flocwolf@gmail.com>
2022-07-12 21:43:36 +0200

    Checks&Balances💲

--- components/Profile/profileCard.js
+++ components/Profile/profileCard.js
@@ -1,3 +1,6 @@
+import { skipToken } from '@reduxjs/toolkit/dist/query'
+import { useState } from 'react'
+import { useGetAllTokenBalancesQuery } from '../../app/covApi'
 import { Avatar } from './Avatar'
 
 
@@ -6,7 +9,7 @@ export const dummyProfile = {
     name: 'CryptoPanda',
     address: '0xd9a51042eBE9A428e362B36F12Bd332bB565deEa',
     avatar: 'https://pbs.twimg.com/profile_images/12098984010/CryptoPanda_400x400.jpg',
-    notes: '',
+    notes: 'My First Note!!!',
     isSelf: false,
     isOneHop: true,
   },
@@ -33,24 +36,50 @@ export const dummyProfile = {
 const ProfileCard = () => {
 
   const [profile, setProfile] = useState(dummyProfile)
+  const { data, loading, error } = useGetAllTokenBalancesQuery({
+    address: profile.bio.address,
+  } || skipToken,
+    {
+      pollingInterval: 300_000, // 5 minutes is the covalent update time
+    })
+
+  function handleChange(e) {
+    e.preventDefault()
+    switch (e.target.id) {
+      case 'notes':
+        setProfile((prevState) => ({ ...prevState, bio: { ...prevState.bio, notes: e.target.value } }))
+      case 'name':
+        setProfile((prevState) => ({ ...prevState, bio: { ...prevState.bio, name: e.target.value } }))
+    }
+  }
 
   return (
     <div
       borderRadius="md"
       className="border-2 self-center grid justify-items-center m-8 p-4 shadow-lg"
     >
-      <div className="w-40 pb-4">
+      <div
+        // TODO: onClick open a mpdal where the user can input a new image URL, which gets previews and upon confirmation, savved to the profile 
+        className="w-40 pb-4"
+      >
         <Avatar w="60" scale={110} />
       </div>
 
-      <div className="text-black text-2xl dark:text-indigo-50 pb-2">
-        {profile.bio.name}
-      </div>
+      <textarea
+        className=" text-center backdrop-blur-xl dark:backdrop-brightness-110 shadow-sm focus:ring-indigo-500 focus:border-indigo-500 flex flex-grow w-fit  border-slate-300 rounded-xl bg-transparent border-0 form-textarea resize-none text-bold text-3xl dark:text-indigo-50 mb-4"
+        rows="1"
+        name="name"
+        id="name"
+        value={profile.bio.name}
+        placeholder="Add a nick-name..."
+        onChange={(e) => handleChange(e)}
+      >
+      </textarea>
       <div className="ml-3 px-2 py-1 bg-indigo-500 bg-opacity-80 rounded-tr-xl rounded-bl-xl text-snow text-xs hover:text-snow-muted hover:text-semibold  hover:bg-indigo-600 transition-colors duration-300 truncate">
         {profile.bio.address}
       </div>
       <div className="text-slate-900 text-semibold text-xl dark:text-snow p-6">
-        {}
+        { }
       </div>
       <div
         // className="mt-1"
@@ -59,20 +88,25 @@ const ProfileCard = () => {
         <textarea
           className="dark:text-snow bg-transparent border-0 rounded-xl p-3 form-textarea flex flex-grow resize-none"
           rows="4"
-          name="comment"
-          id="comment"
+          name="notes"
+          id="notes"
           value={profile.bio.notes}
           placeholder="Add your comment ..."
+          onChange={(e) => handleChange(e)}
         ></textarea>
       </div>
-      {/* <div className="flex-shrink-0 pt-4">
-        <button
-          type="submit"
-          className="inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-        >
-          Submit
-        </button>
-      </div> */}
+      <div>
+        {loading
+          ? <div>Loading...</div>
+          : data &&
+          <div
+          // TODO: for each chain a dropdown with a list of tokens with non-zero balance
+            className='dark:text-indigo-200 w-40 truncate'
+          >
+            {JSON.stringify(data)}
+          </div>
+        }
+      </div>
     </div>
   )
 }

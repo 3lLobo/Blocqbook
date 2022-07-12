753fe77a692c6ff91f5483586b471cbd830623f6
3lLobo <flocwolf@gmail.com>
2022-07-12 18:37:43 +0200

    CovMoneyyy🥘

--- app/covApi.js
+++ app/covApi.js
@@ -7,41 +7,36 @@ const chainIds = [1, 42, 250, 80001]
 
 const axiosBaseQuery =
   ({ baseUrl } = { baseUrl: '' }) =>
-  async ({ url, method, data, params }) => {
-    // This attaches the api key to every request
-    // Assume url is a list of urls
-    if (params) {
-      params['key'] = process.env.NEXT_PUBLIC_COVALENT_API_KEY
-    } else {
-      params = { key: process.env.NEXT_PUBLIC_COVALENT_API_KEY }
-    }
-    try {
-      const result = []
-      for (let x in url) {
-        const res = await axios({
-          url: baseUrl + url[x],
-          method,
-          data,
-          params,
-          // // This should work like CURL using Basic Auth, but it doesn't
-          // auth: {
-          //   username: 'process.env.NEXT_PUBLIC_COVALENT_API_KEY',
-          //   // password: ''
-          // },
-        })
-        result.push(res.data.data)
+    async ({ url, method, data, params }) => {
+      // This attaches the api key to every request
+      // Assume url is a list of urls
+      if (params) {
+        params['key'] = process.env.NEXT_PUBLIC_COVALENT_API_KEY
+      } else {
+        params = { key: process.env.NEXT_PUBLIC_COVALENT_API_KEY }
       }
-      return { data: result }
-    } catch (axiosError) {
-      let err = axiosError
-      return {
-        error: {
-          status: err.response?.status,
-          data: err.response?.data || err.message,
-        },
+      try {
+        const result = []
+        for (let x in url) {
+          const res = await axios({
+            url: baseUrl + url[x],
+            method,
+            data,
+            params,
+          })
+          result.push(res.data.data)
+        }
+        return { data: result }
+      } catch (axiosError) {
+        let err = axiosError
+        return {
+          error: {
+            status: err.response?.status,
+            data: err.response?.data || err.message,
+          },
+        }
       }
     }
-  }
 
 // Define a service using a base URL and expected endpoints
 export const covApi = createApi({
@@ -70,7 +65,26 @@ export const covApi = createApi({
         // }
       }),
     }),
-  }),
+    getTokenBalances: builder.query({
+      query: ({ address, chain_id }) => ({
+        url: [chain_id + `/address/${address}/balances_v2/`],
+        responseHandler: (response) => response[0],
+      }),
+    }),
+    getAllTokenBalances: builder.query({
+      query: ({ address }) => ({
+        url: chainIds.map(
+          (chain_id) => `${chain_id}/address/${address}/balances_v2/`
+        ),
+      }),
+    }),
+  })
 })
 
-export const { useGetTransactionsQuery, useGetAllTransactionsQuery } = covApi
+export const {
+  useGetTransactionsQuery,
+  useGetAllTransactionsQuery,
+  useGetAllTokenBalancesQuery,
+  useLazyGetAllTokenBalancesQuery,
+  useGetTokenBalancesQuery
+} = covApi

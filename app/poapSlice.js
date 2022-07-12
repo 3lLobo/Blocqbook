4601b816996b7c4097aefcd22bbb8221d3825648
Julian <perassijulian@gmail.com>
2022-07-12 17:41:01 -0300

    change to use fetchBaseQuery

--- app/poapSlice.js
+++ app/poapSlice.js
@@ -1,17 +1,22 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
-export const poapSlice = createSlice({
-  name: 'poap',
-  initialState: {
-    events: [],
-  },
-  reducers: {
-    toggleTheme: (state) => {
-      state.events = []
+// Define a service using a base URL and expected endpoints
+export const poapApi = createApi({
+  reducerPath: 'poapApi',
+  baseQuery: fetchBaseQuery({
+    baseUrl: 'https://api.poap.tech/actions/',
+    headers: {
+      Accept: 'application/json',
+      'X-API-Key': NEXT_PUBLIC_POAP_API_KEY,
     },
-  },
+  }),
+  endpoints: (builder) => ({
+    getPoaps: builder.query({
+      query: (address) => `scan/${address}`,
+    }),
+  }),
 })
 
-export const { toggleTheme } = poapSlice.actions
-
-export default poapSlice.reducer
+// Export hooks for usage in functional components, which are
+// auto-generated based on the defined endpoints
+export const { useGetPoapsQuery } = poapApi

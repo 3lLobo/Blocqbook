005c32326a20c404d13dbb419bb52a72aa1efedf
Julian <perassijulian@gmail.com>
2022-07-15 10:39:24 -0300

    retrieving data to UI

--- components/Poap/index.js
+++ components/Poap/index.js
@@ -1,10 +1,30 @@
-import { useGetPoapsQuery } from "../../app/poapApi"
+import { useEffect } from 'react'
+import { useGetPoapsQuery } from '../../app/poapApi'
 
-const Poap = () => {
-    const { data, error, isLoading} = useGetPoapsQuery("pablito.eth")
-    console.log('data:', data);
+const Poap = ({ address }) => {
+  const { data, error, isLoading } = useGetPoapsQuery('pablito.eth')
+  useEffect(() => {
+    if (!isLoading) {
+      console.log('data:', data)
+    }
+    if (error) {
+      console.log(error)
+    }
+  }, [isLoading])
+
   return (
-    <div>Poap</div>
+    <div>
+      {data && (
+        <div className=''>
+          {data.map((d, index) => (
+            <div key={index} className="w-80 bg-white flex flex-col items-center mt-2 p-2 rounded-md">
+              <p className='text-center'>{d.event.name}</p>
+              <img className="w-32 mt-2" src={d.event.image_url} alt="" />
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
   )
 }
 

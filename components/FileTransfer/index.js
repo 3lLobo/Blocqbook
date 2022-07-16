4c1fda568e8870ffb254e104e05d5dc3b2a82c24
Julian <perassijulian@gmail.com>
2022-07-16 11:03:17 -0300

    sending CIDs

--- components/FileTransfer/index.js
+++ components/FileTransfer/index.js
@@ -2,21 +2,22 @@ import { useState } from 'react'
 import { Web3Storage } from 'web3.storage'
 import { Client } from '@xmtp/xmtp-js'
 import { ethers, Wallet } from 'ethers'
+import { BezierSpinner } from '../Spinner/BezierSpinner'
 
 const FileTransfer = () => {
   const token = process.env.NEXT_PUBLIC_WEB3STORAGE
   const [files, setFiles] = useState([])
+  const [filesCID, setFilesCID] = useState([])
   const [address, setAddress] = useState('')
+  const [isUploading, setIsUploading] = useState(false)
 
-  //XMTP
-
-  async function handleSubmit(event) {
+  async function handleUpload(event) {
     event.preventDefault()
-
-    setFiles(event.target.files)
+    setIsUploading(true);
+    console.log('files:', files);
 
     console.log('> 📦 creating web3.storage client')
-    const client = new Web3Storage({ token })
+    const client = await new Web3Storage({ token })
 
     console.log(
       '> 🤖 chunking and hashing the files to calculate the Content ID'
@@ -31,6 +32,7 @@ const FileTransfer = () => {
     })
     console.log(`> ✅ web3.storage now hosting ${cid}`)
     console.log(`https://dweb.link/ipfs/${cid}`)
+    setFilesCID(cid);
 
     console.log('> 📡 fetching the list of all unique uploads on this account')
     let totalBytes = 0
@@ -39,6 +41,7 @@ const FileTransfer = () => {
       totalBytes += upload.dagSize || 0
     }
     console.log(`> ⁂ ${totalBytes.toLocaleString()} bytes stored!`)
+    setIsUploading(false);
   }
 
   const sendFile = async () => {
@@ -51,13 +54,9 @@ const FileTransfer = () => {
     // You'll want to replace this with a wallet from your application
     const wallet = provider.getSigner()
     // Create the client with your wallet. This will connect to the XMTP development network by default
-    console.log('wallet:', await wallet.getAddress())
     const xmtp = await Client.create(wallet)
     // Start a conversation with Vitalik
-    const conversation = await xmtp.conversations.newConversation(
-      // '0xf33528c60aEd847BB1E4bE08953C25C7eaB74A7F'
-      '0xbf3f8D6a3aE5cfc144AA116896b82F3a87671F83' //MUMBAI
-    )
+    const conversation = await xmtp.conversations.newConversation(address)
     // Load all messages in the conversation
     console.log('Loading messages...')
     const messages = await conversation.messages()
@@ -69,7 +68,7 @@ const FileTransfer = () => {
 
     // Send a message
     console.log('Sending message...')
-    await conversation.send('This is 6th i think')
+    await conversation.send(filesCID)
     console.log('Message sent.')
     // // Listen for new messages in the conversation
     // console.log('Loading conversations...')
@@ -83,17 +82,18 @@ const FileTransfer = () => {
 
   return (
     <div className="flex justify-center">
-      <div className='flex flex-col items-center gap-8 mt-4'>
+      <div className="flex flex-col items-center gap-8 mt-4">
         <div className="text-2xl">Send files to your friends</div>
         <div className="">
           <label htmlFor="filePicker">Pick files to send</label>
           <input
             type="file"
             name="filePicker"
-            onChange={(e) => handleSubmit(e)}
+            onChange={(e) => setFiles(e.target.files)}
             multiple
             required
           />
+          {files.length !== 0 && <button onClick={handleUpload}>Upload icon</button>}
         </div>
         <div className="">
           <label htmlFor="addressPicker">Pick your friend</label>
@@ -101,12 +101,12 @@ const FileTransfer = () => {
             type="text"
             name="addressPicker"
             placeholder="for now just paste but should be a selector"
-            onChange={(e) => handleSubmit(e)}
+            onChange={(e) => setAddress(e.target.value)}
             required
           />
         </div>
         <div></div>
-        <button onClick={sendFile}>SEND FILE</button>
+        <button disabled={isUploading} onClick={sendFile}>{isUploading? <BezierSpinner /> : "SEND FILE"}</button>
       </div>
     </div>
   )

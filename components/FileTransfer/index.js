8a42acc2719f0d25eaecfff1c01b63e270eef333
Julian <perassijulian@gmail.com>
2022-07-16 14:22:45 -0300

    receiving cid and displaying them

--- components/FileTransfer/index.js
+++ components/FileTransfer/index.js
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { Web3Storage } from 'web3.storage'
 import { Client } from '@xmtp/xmtp-js'
 import { ethers, Wallet } from 'ethers'
@@ -6,15 +6,21 @@ import { BezierSpinner } from '../Spinner/BezierSpinner'
 
 const FileTransfer = () => {
   const token = process.env.NEXT_PUBLIC_WEB3STORAGE
+
   const [files, setFiles] = useState([])
   const [filesCID, setFilesCID] = useState([])
   const [address, setAddress] = useState('')
   const [isUploading, setIsUploading] = useState(false)
+  const [receivedMedia, setReceivedMedia] = useState([])
+  const contactBook = [
+    '0x31ca5fF81B577216e038412DD879b998ae7b71Db',
+    '0x5BCA9820F9e70B211055F96aB88e7103D5C304D2',
+  ]
 
   async function handleUpload(event) {
     event.preventDefault()
-    setIsUploading(true);
-    console.log('files:', files);
+    setIsUploading(true)
+    console.log('files:', files)
 
     console.log('> 📦 creating web3.storage client')
     const client = await new Web3Storage({ token })
@@ -32,7 +38,7 @@ const FileTransfer = () => {
     })
     console.log(`> ✅ web3.storage now hosting ${cid}`)
     console.log(`https://dweb.link/ipfs/${cid}`)
-    setFilesCID(cid);
+    setFilesCID(cid)
 
     console.log('> 📡 fetching the list of all unique uploads on this account')
     let totalBytes = 0
@@ -41,34 +47,25 @@ const FileTransfer = () => {
       totalBytes += upload.dagSize || 0
     }
     console.log(`> ⁂ ${totalBytes.toLocaleString()} bytes stored!`)
-    setIsUploading(false);
+    setIsUploading(false)
   }
 
   const sendFile = async () => {
-    // console.log(window.ethereum)
     const provider = new ethers.providers.Web3Provider(window.ethereum)
     await provider.send('eth_requestAccounts', [])
-    // const signer = provider.getSigner()
-    // const add = await signer.getAddress()
-    // console.log('signer:', add);
-    // You'll want to replace this with a wallet from your application
     const wallet = provider.getSigner()
-    // Create the client with your wallet. This will connect to the XMTP development network by default
     const xmtp = await Client.create(wallet)
-    // Start a conversation with Vitalik
     const conversation = await xmtp.conversations.newConversation(address)
-    // Load all messages in the conversation
+
     console.log('Loading messages...')
     const messages = await conversation.messages()
     for await (const message of messages) {
       console.log(`[${message.senderAddress}]: ${message.content}`)
     }
-    console.log('Messages loaded.')
-    console.log('messages:', messages)
 
     // Send a message
     console.log('Sending message...')
-    await conversation.send(filesCID)
+    await conversation.send('/mediafile/' + filesCID)
     console.log('Message sent.')
     // // Listen for new messages in the conversation
     // console.log('Loading conversations...')
@@ -80,8 +77,38 @@ const FileTransfer = () => {
     // console.log('Conversations loaded.')
   }
 
+  const getFiles = async () => {
+    const provider = new ethers.providers.Web3Provider(window.ethereum)
+    await provider.send('eth_requestAccounts', [])
+    const wallet = provider.getSigner()
+    const xmtp = await Client.create(wallet)
+    for await (const contact of contactBook) {
+      const conversation = await xmtp.conversations.newConversation(contact)
+      console.log(`Loading messages from ${contact}...`)
+      const messages = await conversation.messages()
+      for await (const message of messages) {
+        const sliced = message.content.slice(0, 11)
+        if (
+          (sliced === '/mediafile/') &
+          (message.senderAddress !== wallet.getAddress())
+        ) {
+          const cid = message.content.slice(-59)
+          const sender = message.senderAddress
+          setReceivedMedia(prevState => [...prevState, {sender, cid}])
+        }
+      }
+    }
+    console.log('receivedMedia FINAL:', receivedMedia)
+    console.log('receivedMedia.length:', receivedMedia.length);
+  }
+
+  // useEffect(() => {
+  //   getFiles()
+  // }, [])
+  
+
   return (
-    <div className="flex justify-center">
+    <div className="flex flex-col items-center">
       <div className="flex flex-col items-center gap-8 mt-4">
         <div className="text-2xl">Send files to your friends</div>
         <div className="">
@@ -93,7 +120,9 @@ const FileTransfer = () => {
             multiple
             required
           />
-          {files.length !== 0 && <button onClick={handleUpload}>Upload icon</button>}
+          {files.length !== 0 && (
+            <button onClick={handleUpload}>Upload icon</button>
+          )}
         </div>
         <div className="">
           <label htmlFor="addressPicker">Pick your friend</label>
@@ -106,7 +135,23 @@ const FileTransfer = () => {
           />
         </div>
         <div></div>
-        <button disabled={isUploading} onClick={sendFile}>{isUploading? <BezierSpinner /> : "SEND FILE"}</button>
+        <button
+          disabled={isUploading || filesCID.length === 0}
+          onClick={sendFile}
+        >
+          {isUploading ? <BezierSpinner /> : 'SEND FILE'}
+        </button>
+      </div>
+      <div className="flex flex-col items-center gap-8 mt-8">
+        <div className="text-2xl">Files received</div>
+        {receivedMedia.length > 0 &&
+          receivedMedia.map((m, i) => (
+            <div key={i} className="flex flex-col">
+              <div>{m.sender}</div>
+              <div>{m.cid}</div>
+            </div>
+          ))}
+        <button onClick={getFiles}>get</button>
       </div>
     </div>
   )
